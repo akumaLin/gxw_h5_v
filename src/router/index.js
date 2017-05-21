b0cae36d60028c8dbfca4c913b5b008864269d5e
linyongxin <172754827@qq.com>
2017-05-21 21:52:57 +0800

    重构测试了

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue      from 'vue'
 import Router   from 'vue-router'
-/*import myPrize  from '@/pages/my-prize/my-prize.vue'*/
-const myPrize= r => require.ensure([], () => r(require('@/pages/my-prize/my-prize.vue')), 'group-myPrize')
-/*import newPeople  from '@/pages/new-people/new-people.vue'*/
-const newPeople= r => require.ensure([], () => r(require('@/pages/new-people/new-people.vue')), 'group-newPeople')
-/*import home  from '@/pages/home/home.vue'*/
-const home= r => require.ensure([], () => r(require('@/pages/home/home.vue')), 'group-home')
+import myPrize  from '@/pages/my-prize/my-prize.vue'
+/*const myPrize= r => require.ensure([], () => r(require('@/pages/my-prize/my-prize.vue')), 'group-myPrize')*/
+import newPeople  from '@/pages/new-people/new-people.vue'
+/*const newPeople= r => require.ensure([], () => r(require('@/pages/new-people/new-people.vue')), 'group-newPeople')*/
+import home  from '@/pages/home/home.vue'
+/*const home= r => require.ensure([], () => r(require('@/pages/home/home.vue')), 'group-home')*/
 Vue.use(Router)
 
 export default new Router({
